b25136b4cee2f838f30ab6d07fe879cac9f281f1
Catalina Cueto (Cat) <catacueto@gmail.com>
2022-08-13 08:14:03 -0400

    finished postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -19,7 +19,8 @@ router.post("/", withAuth, async (req, res) => {
 // Route to delete post by id
 router.delete("/:id", withAuth, async (req, res) => {
   try {
-    const deletePost = await Project.destroy({
+    // destroy post where id
+    const deletePost = await Post.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
@@ -31,7 +32,27 @@ router.delete("/:id", withAuth, async (req, res) => {
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(deletePost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Route to update a post
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    // updated post grabs from req.body
+    const updatedPost = {
+      post_title: req.body.title,
+      post_content: req.body.post_content,
+    };
+    // updates in db
+    const updated = await Post.update(updatedPost, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: updated });
   } catch (err) {
     res.status(500).json(err);
   }
